79a12d9ed89e62490e4082451fc748caf3d66a10
Coby Brown <tankz135yt@gmail.com>
2025-07-02 16:31:48 -0400

    Working on changing from Python to Java

---
+++ MusicList.java
@@ -0,0 +1,8 @@
+//go by [number,filename,author,origin,type]
+public class MusicList{
+    String[][] MusicList = { 
+        {"1","01. Firebugs.mp3","Lena Raine","Minecraft","Ambient"}, 
+        {"2","16. Relic.mp3","Aaron Cherof","Minecraft","Electronic"}, 
+        {"3","Mega Man Battle Network OST - T17 Net Battle (Boss Theme) - Frygoon.mp3","Akari Kaida","MegaMan","Chiptune"}
+    };
+}
